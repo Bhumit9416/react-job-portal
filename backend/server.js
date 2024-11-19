d6d414f80deb17038640f6621d8ce489332ea7f1
Bhumit Mittal <bhumit9416@gmail.com>
2024-11-19 13:24:32 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
-import app from "./app.js";
-import cloudinary from "cloudinary";
+import dotenv from 'dotenv';
+import cloudinary from 'cloudinary';
+import app from './app.js';
 
+// Load environment variables from .env file
+dotenv.config();
+
+// Configure Cloudinary with the environment variables
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
-  api_key: process.env.CLOUDINARY_CLIENT_API,
-  api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,  // use the name you defined in .env
+  api_key: process.env.CLOUDINARY_API_KEY,       // use the name you defined in .env
+  api_secret: process.env.CLOUDINARY_API_SECRET, // use the name you defined in .env
 });
 
+// Start the server
 app.listen(process.env.PORT, () => {
   console.log(`Server running at port ${process.env.PORT}`);
 });
